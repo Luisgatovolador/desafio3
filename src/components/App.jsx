c3208014f656d8db9b302839b6f9d264aa037491
Luisgatovolador <rodriguez.ruisu@gmail.com>
2025-06-13 20:32:41 -0600

    Primer commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Mascotas from './Mascotas';
+import './App.css';
+
+function Home() {
+    return <h1>Bienvenidos a la Página de Inicio</h1>;
+}
+
+function About() {
+    return <h2>Acerca de Nosotros</h2>;
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="mascotas" element={<Mascotas />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
